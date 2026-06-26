d358b91fd5a2d3c59532387a7916b429b5b671b1
sandeepgaikwad-network <sandeepgaikwad.network@gmail.com>
2026-06-26 13:58:35 +0530

    Step 6: Added Virtual Machine

---
+++ virtual_machine.tf
@@ -0,0 +1,28 @@
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "vm-project-dev-southindia"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  os_disk {
+    name              = "osdisk-project-dev-southindia"
+    caching           = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+}
